b64508b5daf81ddbc8bde47dfd34e2073f85204a
guolei <174000902@qq.com>
2024-07-12 15:37:06 +0800

    update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
     name="guolei-py3-brhk",
-    version="0.0.4",
+    version="0.0.5",
     description="天津博瑞皓科 API",
     long_description=long_description,
     long_description_content_type="text/markdown",
