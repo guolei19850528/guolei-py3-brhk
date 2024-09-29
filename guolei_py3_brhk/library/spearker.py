79d4d565157e45475da3feac059817ee0ae18be1
guolei <174000902@qq.com>
2024-09-29 10:03:06 +0800

    update

--- guolei_py3_brhk/library/spearker.py
+++ guolei_py3_brhk/library/spearker.py
@@ -8,15 +8,41 @@ Email：[174000902@qq.com]
 Github：https://github.com/guolei19850528/guolei_py3_brhk
 =================================================
 """
-from typing import Callable
+from typing import Callable, Any
 
 import requests
 from addict import Dict
+from guolei_py3_requests.library import ResponseCallable, request
 from jsonschema.validators import validate, Draft202012Validator
+from requests import Response
 
 
-class ApiUrlSettings:
-    URL__NOTIFY = "/notify.php"
+class ResponseCallable(ResponseCallable):
+    """
+    Response Callable Class
+    """
+
+    @staticmethod
+    def json_addict__errcode_is_0(response: Response = None, status_code: int = 200):
+        json_addict = ResponseCallable.json_addict(response=response, status_code=status_code)
+        if Draft202012Validator({
+            "type": "object",
+            "properties": {
+                "errcode": {
+                    "oneOf": [
+                        {"type": "integer", "const": 0},
+                        {"type": "string", "const": "0"},
+                    ],
+                },
+            },
+            "required": ["errcode"]
+        }).is_valid(json_addict):
+            return True
+        return False
+
+
+class UrlsSetting:
+    NOTIFY = "/notify.php"
 
 
 class Api(object):
@@ -82,100 +108,45 @@ class Api(object):
 
     def post(
             self,
-            url: str = "",
-            params: dict = None,
-            data: dict = None,
-            kwargs: dict = None,
-            custom_callable: Callable = None
+            response_callable: Callable = ResponseCallable.json_addict__errcode_is_0,
+            url: str = None,
+            params: Any = None,
+            data: Any = None,
+            json: Any = None,
+            headers: Any = None,
+            **kwargs: Any
     ):
-        """
-        use requests.post
-        :param url: requests.post(url=url,params=params,data=data,**kwargs) url=base_url+url if not pattern ^http else url
-        :param params: requests.post(url=url,params=params,data=data,**kwargs)
-        :param data: requests.post(url=url,params=params,data=data,**kwargs)
-        :param kwargs: requests.post(url=url,params=params,data=data,**kwargs)
-        :param custom_callable: custom_callable(response) if isinstance(custom_callable,Callable)
-        :return:custom_callable(response) if isinstance(custom_callable,Callable) else addict.Dict instance
-        """
-        if not Draft202012Validator({"type": "string", "minLength": 1, "pattern": "^http"}).is_valid(url):
-            url = f"/{url}" if not url.startswith("/") else url
-            url = f"{self.base_url}{url}"
-        kwargs = Dict(kwargs) if isinstance(kwargs, dict) else Dict()
-        response = requests.post(
+        return self.request(
+            response_callable=response_callable,
+            method="POST",
             url=url,
             params=params,
             data=data,
-            **kwargs.to_dict()
+            json=json,
+            headers=headers,
+            **kwargs
         )
-        if isinstance(custom_callable, Callable):
-            return custom_callable(response)
-        if response.status_code == 200:
-            json_addict = Dict(response.json())
-            if Draft202012Validator({
-                "type": "object",
-                "properties": {
-                    "errcode": {
-                        "oneOf": [
-                            {"type": "integer", "const": 0},
-                            {"type": "string", "const": "0"},
-                        ],
-                    },
-                    "errmsg": {"type": "string", "enums": ["ok", "OK", "Ok", "oK"]}
-                },
-                "required": ["errcode", "errmsg"]
-            }).is_valid(json_addict):
-                return True
-        return False
 
     def request(
             self,
+            response_callable: Callable = ResponseCallable.json_addict__errcode_is_0,
             method: str = "GET",
-            url: str = "",
-            params: dict = None,
-            data: dict = None,
-            kwargs: dict = None,
-            custom_callable: Callable = None
+            url: str = None,
+            params: Any = None,
+            headers: Any = None,
+            **kwargs
     ):
-        """
-        use requests.request
-        :param method: requests.request(method=method,url=url,params=params,data=data,**kwargs)
-        :param url: requests.request(method=method,url=url,params=params,data=data,**kwargs) url=base_url+url if not pattern ^http else url
-        :param params: requests.request(method=method,url=url,params=params,data=data,**kwargs)
-        :param data: requests.request(method=method,url=url,params=params,data=data,**kwargs)
-        :param kwargs: requests.request(method=method,url=url,params=params,data=data,**kwargs)
-        :param custom_callable: custom_callable(response) if isinstance(custom_callable,Callable)
-        :return:custom_callable(response) if isinstance(custom_callable,Callable) else addict.Dict instance
-        """
         if not Draft202012Validator({"type": "string", "minLength": 1, "pattern": "^http"}).is_valid(url):
             url = f"/{url}" if not url.startswith("/") else url
             url = f"{self.base_url}{url}"
-        kwargs = Dict(kwargs) if isinstance(kwargs, dict) else Dict()
-        response = requests.request(
+        return request(
+            response_callable=response_callable,
             method=method,
             url=url,
             params=params,
-            data=data,
-            **kwargs.to_dict()
+            headers=headers,
+            **kwargs
         )
-        if isinstance(custom_callable, Callable):
-            return custom_callable(response)
-        if response.status_code == 200:
-            json_addict = Dict(response.json())
-            if Draft202012Validator({
-                "type": "object",
-                "properties": {
-                    "errcode": {
-                        "oneOf": [
-                            {"type": "integer", "const": 0},
-                            {"type": "string", "const": "0"},
-                        ],
-                    },
-                    "errmsg": {"type": "string", "enums": ["ok", "OK", "Ok", "oK"]}
-                },
-                "required": ["errcode", "errmsg"]
-            }).is_valid(json_addict):
-                return True
-        return False
 
     def notify(
             self,
@@ -187,4 +158,8 @@ class Api(object):
         data.setdefault("id", self.id)
         data.setdefault("version", self.version)
         data.setdefault("message", message)
-        return self.post(url=ApiUrlSettings.URL__NOTIFY, data=data.to_dict())
+        return self.post(
+            response_callable=ResponseCallable.json_addict__errcode_is_0,
+            url=UrlsSetting.URL__NOTIFY,
+            data=data.to_dict()
+        )
